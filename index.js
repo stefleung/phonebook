c61ee0d261f2aeda5b9baea4b5f4e34cc28a295d
Stefani Leung <stephkt@gmail.com>
2024-02-12 11:12:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+morgan.token("body", (req, res) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const info = () => `<p>Phonebook has info for ${persons.length} people</p>
+<p>${Date()}</p>
+`;
+
+app.get("/info", (request, response) => {
+  response.send(info());
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  if (!person.name) {
+    return response.status(400).json({
+      error: "name is missing",
+    });
+  } else if (!person.number) {
+    return response.status(400).json({
+      error: "number is missing",
+    });
+  } else if (persons.filter((p) => p.name === person.name).length !== 0) {
+    return response.status(400).json({
+      error: "name exists in phone book already",
+    });
+  }
+  const id = Math.floor(Math.random() * 1000000);
+  person.id = id;
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
